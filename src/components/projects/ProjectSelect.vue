60a90a4f2051da5a0ec6c377f45de56363b61c64
Eugene D <42403848+fervark@users.noreply.github.com>
2022-09-19 10:05:33 +0300

    Base structure

---
+++ src/components/projects/ProjectSelect.vue
@@ -0,0 +1,20 @@
+<script>
+import BaseSelect from "@/components/base/input/BaseSelect.vue";
+export default {
+  name: "ProjectSelect",
+  components: {BaseSelect},
+  data() {
+    return {
+      projects: [
+        {id: 1, name: 'Проект Wiki'},
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <BaseSelect :item-data="projects" field-name="projects_select" field-id="projects_select" />
+  </div>
+</template>
