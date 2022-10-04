5f77b1131991d836d27162dc10584293e9e7dcf7
Eugene D <42403848+fervark@users.noreply.github.com>
2022-10-04 12:04:41 +0300

    Fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import {fileURLToPath, URL} from 'url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const apiDomain = 'https://1044631-cr49176.tmweb.ru/'
+const apiDomain = 'http://1044631-cr49176.tmweb.ru'
 
 // https://vitejs.dev/config/
 export default defineConfig({
